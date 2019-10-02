e948be1dbf5693be763d9e03b8529d2ebc9da31c
Lordwales <tofadeolawale@gmail.com>
2019-10-02 02:48:08 +0100

    Added Swagger to test the api created

--- Backend/Startup.cs
+++ Backend/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace Backend
 {
@@ -29,6 +30,12 @@ namespace Backend
             });
             
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Conference Planner API", Version = "v1" });
+            });
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +45,13 @@ namespace Backend
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Conference Planner API v1");
+            });
 
             app.UseHttpsRedirection();
 
@@ -49,6 +63,10 @@ namespace Backend
             {
                 endpoints.MapControllers();
             });
+
+
+            
+            
         }
     }
 }
--- Backend/readme.md
+++
@@ -1,1 +0,0 @@
-This is my first asp.net app. Just learning.
